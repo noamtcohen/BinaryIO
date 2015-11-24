f11ee3fb0aadb64f676b6a46f09d8a72fd3d28da
Noam Cohen <noam@ecb.co.il>
2015-11-24 15:34:49 +0200

    add icon

--- bio/browser-node.js
+++ bio/browser-node.js
@@ -1,7 +1,9 @@
 /**
  * Created by noamc on 11/24/15.
  */
-if(typeof require === "undefined")
-    window.require = function(module){return window};
+
 if(typeof exports === "undefined")
-    window.exports = undefined;
+    window.exports = window;
+
+if(typeof require === "undefined")
+    window.require = function(module){return window.exports};
